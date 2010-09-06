f3c9a1d87f5b69f33cca4b77ae5ed1672204b4fe
Tony Day <tony@lazylabs.com>
2010-09-06 23:03:34 +1200

    More tweaking.

--- client.rb
+++ client.rb
@@ -8,14 +8,14 @@ client = Gearman::Client.new('localhost')
 taskset = Gearman::TaskSet.new(client)
 
 n = 0
-while !@stopped
+while !@stopped && n < 1000
   n += 1
   data = sprintf("%07d - %s", n, Time.now.strftime('%M:%S'))
   puts data
   task = Gearman::Task.new('upper', data, :background => true)
   taskset.add_task(task)
-  taskset.wait(0)
-#  sleep 0.1 unless @stopped
+  taskset.wait(1_000_000)
+  sleep 1 unless @stopped
 end
 
 puts 'All done - thanks for playing'
