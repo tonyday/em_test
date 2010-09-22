d5cdc990860957ef6640200b072b2e43af2355f3
Tony Day <tony@lazylabs.com>
2010-09-22 17:20:45 +1200

    Set "uniq".

--- client.rb
+++ client.rb
@@ -2,20 +2,14 @@
 require 'rubygems'
 require 'gearman'
 
-Signal.trap(:INT) { puts 'setting @stopped'; @stopped = true }
-
 client = Gearman::Client.new('localhost')
-taskset = Gearman::TaskSet.new(client)
-
-n = 0
-while !@stopped && n < 1000
-  n += 1
-  data = sprintf("%07d - %s", n, Time.now.strftime('%M:%S'))
-  puts data
-  task = Gearman::Task.new('upper', data, :background => true)
+timestamp = Time.now.strftime('%Y%m%d%H%M%S')
+(1..1000).each do |n|
+  taskset = Gearman::TaskSet.new(client)
+  data = "Hello, World - #{n}"
+  uniq = "#{timestamp} - #{n}"
+  task = Gearman::Task.new('upper', data, :background => true, :uniq => uniq)
   taskset.add_task(task)
-  taskset.wait(1_000_000)
-  sleep 1 unless @stopped
+  taskset.wait(0)
 end
-
-puts 'All done - thanks for playing'
+puts 'Done !'
